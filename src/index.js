6d2dc4b3f6d0d66b2644127c61927d8eaf0b4bee
mesh-dell <mwaimonicas@gmail.com>
2024-06-22 21:50:46 +0300

    feat(index): Add functionality to commander action
    Changed start date and end date top options
    Added Limit option to limit number of repos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Command } = require("commander");
+const { getProjects } = require("../api/getProjects");
 const program = new Command();
 
 program
@@ -11,15 +12,18 @@ program
 program
   .command("starscope")
   .description("Print the most starred GitHub projects in a date range.")
-  .argument(
-    "<start-date>",
-    "Defines the beginning of the time period for which to retrieve starred project data"
-  )
-  .argument(
-    "<end-date>",
-    "Defines the end of the time period for which to retrieve starred project data"
-  )
-  .option("--first", "display just the first project")
-  .action((startDate, endDate) => {});
+  .option("-s, --start-date <date>", "start date (YYYY-MM-DD)")
+  .option("-e, --end-date <date>", "end date (YYYY-MM-DD)")
+  .option("-l, --limit <number>", "number of results to return", "10")
+  .action(async (options) => {
+    const { startDate, endDate, limit } = options;
+    const topProjects = await getProjects(
+      startDate || "2023-01-01",
+      endDate || new Date().toISOString().split("T")[0],
+      limit
+    );
+
+    console.log(topProjects);
+  });
 
 program.parse();
